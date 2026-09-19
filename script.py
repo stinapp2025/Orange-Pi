643fd876aec4ec9e733eb94e26d89c211fdcbeeb
stinapp2025 <stinapp2025@udec.cl>
2026-09-19 17:44:44 -0300

    test de pull en orange pi

--- script.py
+++ script.py
@@ -1,1 +1,25 @@
-print("Hola papi")
+import wiringpi
+import time
+
+# Inicializar el sistema usando la numeración wPi
+wiringpi.wiringPiSetup()
+
+# El Pin físico 7 es el wPi 2
+LED = 2
+
+# Configurar el pin como salida (1 = Out)
+wiringpi.pinMode(LED, 1)
+
+print("Iniciando parpadeo... Presiona Ctrl+C para detener.")
+
+try:
+    while True:
+        wiringpi.digitalWrite(LED, 1)  # Enviar voltaje (Encender)
+        time.sleep(0.5)  # Esperar medio segundo
+        wiringpi.digitalWrite(LED, 0)  # Cortar voltaje (Apagar)
+        time.sleep(0.5)  # Esperar medio segundo
+
+except KeyboardInterrupt:
+    # Al presionar Ctrl+C, nos aseguramos de que el LED quede apagado
+    wiringpi.digitalWrite(LED, 0)
+    print("\nPrograma terminado.")
